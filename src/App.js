8e70112359c722aa28535dcb1360e300885f4121
alok kumar <alok60541@gmail.com>
2024-06-03 22:48:50 +0530

    tailwind + router setup, basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="App">
-      Namaste Everyone , let's build netflix gpt
-    </div>
+    <Body />
   );
 }
 
