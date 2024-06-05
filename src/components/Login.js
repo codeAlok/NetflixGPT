34b2ac1d45d1505eb06289ba37e5e524507922ff
alok kumar <alok60541@gmail.com>
2024-06-05 22:13:46 +0530

    form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,38 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+  }
+
+  // **** validate form data ****
+  const handleButtonClick = () => {
+    // console.log(email);  // gives a object containing value & other data1
+
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
   }
 
   return (
     <div className="w-[100%] h-[100vh] bg-black border-2 border-red-500">
       <Header />
 
-      <div className="absolute opacity-70">
-        <img className="w-[100%] h-[100vh]" src="https://assets.nflxext.com/ffe/siteui/vlv3/dd4dfce3-1a39-4b1a-8e19-b7242da17e68/86742114-c001-4800-a127-c9c89ca7bbe4/IN-en-20240527-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="bg_img" />
+      <div className="absolute opacity-70 w-full">
+        <img className="w-full h-[100vh] object-cover" src="https://assets.nflxext.com/ffe/siteui/vlv3/dd4dfce3-1a39-4b1a-8e19-b7242da17e68/86742114-c001-4800-a127-c9c89ca7bbe4/IN-en-20240527-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="bg_img" />
       </div>
 
-      <form className="w-[30%] absolute bg-black bg-opacity-40 mt-20 p-10 left-0 right-0 mx-auto">
+      <form 
+        onSubmit={(e) => e.preventDefault()}
+        className="w-[30%] absolute bg-black bg-opacity-40 mt-20 p-10 left-0 right-0 mx-auto"
+      >
         <h1 className="text-white text-3xl font-bold mb-6">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -29,17 +45,24 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-3 outline-white mb-4 w-full bg-gray-500 text-white rounded"
         />
         <input
+          ref={password}
           type="password"
           placeholder="password"
           className="p-3 outline-white mb-4 w-full bg-gray-500 text-white rounded"
         />
 
-        <button className="w-full p-2 mt-4 bg-red-600 text-white font-bold rounded ">
+        <p className="text-red-500 font-semibold">{errorMessage}</p>
+        
+        <button
+          className="w-full p-2 mt-4 bg-red-600 text-white font-bold rounded "
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
 
         </button>
