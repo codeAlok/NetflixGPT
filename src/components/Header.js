a23ef5f35f55d9eeea2b192e2b66069106530b5c
alok kumar <alok60541@gmail.com>
2024-06-08 14:20:39 +0530

    bug fix: redirect on login page, if not signed in and try to access browse page & vice versa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
-import { signOut } from "firebase/auth";
-import { useSelector } from "react-redux";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
+  const dispatch = useDispatch();
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
       // Sign-out successful.
-      navigate("/");
     }).catch((error) => {
       // An error happened.
       navigate("/error");
     });
   }
+
+  useEffect(() => {
+    // ** from firebase auth->manage_users
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({
+          uid: uid,
+          email: email,
+          displayName: displayName,
+          photoURL: photoURL
+        }));
+
+        navigate("/browse");
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+  }, []);
 
 
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -7,14 +7,12 @@ import {
   updateProfile
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const email = useRef(null);
@@ -57,8 +55,6 @@ const Login = () => {
                   photoURL: photoURL
                 })
               );
-
-              navigate("/browse");
             })
             .catch((error) => {
               setErrorMessage(error.message);
@@ -76,7 +72,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in 
           const user = userCredential.user;
-          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
