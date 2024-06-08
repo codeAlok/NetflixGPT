bde2eccaab290557ac084826f295c38b5ac37d2d
alok kumar <alok60541@gmail.com>
2024-06-08 23:04:01 +0530

    fetching nowPlaying movies api from TMDB

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,13 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 
 export const USER_AVTR = "https://avatars.githubusercontent.com/u/82399781?v=4";
 
-export const Login_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/dd4dfce3-1a39-4b1a-8e19-b7242da17e68/86742114-c001-4800-a127-c9c89ca7bbe4/IN-en-20240527-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+export const Login_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/dd4dfce3-1a39-4b1a-8e19-b7242da17e68/86742114-c001-4800-a127-c9c89ca7bbe4/IN-en-20240527-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+// *** For api call from TMDB (containing access token) ****
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjhhMWJjNTNkN2M5OTA0ZDU3NWI1MWFjNWQ4ZGQ5OCIsInN1YiI6IjY2NjQ3NzBmZGE1NGVhZmM4MTRkYjRiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.31okL-DMf-3vlXgkUZtuu-EGRGzytL0lgIwHn3GQJ6g'
+    }
+};
